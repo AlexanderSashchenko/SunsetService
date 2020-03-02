f09665db7ddfdd2af91d9b2d1bd9ef15ca3b9523
Alexandr Sashchenko <santys87@gmail.com>
2020-03-02 13:50:58 +0200

    first commit

---
+++ src/main/java/com/datax/sunset/SunsetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.datax.sunset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunsetServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datax/sunset/SunsetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datax.sunset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
